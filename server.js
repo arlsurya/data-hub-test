ba4a4aa7ad7bbc1625e5276ded79bf678d375317
arlsurya <suryaaryal204@gmail.com>
2023-10-13 14:50:03 +0545

    port updated

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./app')
 
 global.appRoot = path.resolve(__dirname)
 console.log(appRoot)
-app.listen(3001,()=>{
-    console.log("server is running on 127.0.0.1:3001")
+app.listen(8080,()=>{
+    console.log("server is running on 127.0.0.1:8080")
 })
