b44729d32923647b82863dc124ea6e8c91e232a5
arlsurya <suryaaryal204@gmail.com>
2023-10-14 21:05:19 +0545

    cluster method implement

--- app.js
+++ app.js
@@ -10,67 +10,100 @@ const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const path = require('path');
-const moment = require('moment-timezone'); // Import the moment-timezone library
+const moment = require('moment-timezone');
 const { secondaryDB } = require('./Core/atlasdb');
+const cluster = require('cluster');
+const os = require('os');
 
-app.use(cors());
-app.use(fileUpload());
-DB();
-app.disable("x-powered-by");
-app.use(express.json());
+const numCPUs = os.cpus().length;
 
-// Set the Nepal time zone
-const nepalTimeZone = 'Asia/Kathmandu';
-
-// Create a custom token for the formatted date
-morgan.token('nepal-time', (req, res, tz) => {
-    return moment().tz(tz).format('YYYY-MM-DD HH:mm:ss');
-});
-
-// Define a custom format that includes the Nepal time zone-formatted date
-const customFormat = ':remote-addr - [:nepal-time[' + nepalTimeZone + ']] ":method :url" :status :response-time ms ":user-agent"';
-
-// Create a write stream for the log file
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+console.log("Total CPUs => " + numCPUs)
+console.log("Process Id => " + process.pid)
 
 
-app.use(morgan(customFormat, {
-    stream: accessLogStream
-}));
+if (cluster.isMaster) {
+    // Create a cluster of worker processes
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-// Create a middleware to save log data to the secondary MongoDB database
-app.use((req, res, next) => {
-    const responseTime = parseFloat(res.get('response-time'));
+    cluster.on('exit', (worker, code, signal) => {
+        // Replace the terminated worker
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+} else {
+    // Worker code
+    app.use(cors());
+    app.use(fileUpload());
+    DB();
+    app.disable('x-powered-by');
+    app.use(express.json());
 
-    const logData = {
-        remoteAddr: req.ip,
-        method: req.method,
-        url: req.originalUrl,
-        status: res.statusCode,
-        responseTime: responseTime,
-        userAgent: req.get('user-agent'),
-        timestamp: moment().tz(nepalTimeZone).format('YYYY-MM-DD HH:mm:ss')
-    };
+    // Set the Nepal time zone
+    const nepalTimeZone = 'Asia/Kathmandu';
 
-    // collection name accesslogs
-    const secondaryCollection = secondaryDB.collection('accessLogs');
-    secondaryCollection.insertOne(logData, (error, result) => {
-        if (error) {
-            console.error('Error saving log data to the secondary database:', error);
-        } else {
-            console.log('Log data saved to the secondary database:', result);
-        }
+    // Create a custom token for the formatted date
+    morgan.token('nepal-time', (req, res, tz) => {
+        return moment().tz(tz).format('YYYY-MM-DD HH:mm:ss');
     });
 
-    next();
-});
+    // Define a custom format that includes the Nepal time zone-formatted date
+    const customFormat =
+        ':remote-addr - [:nepal-time[' + nepalTimeZone + ']] ":method :url" :status :response-time ms ":user-agent"';
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-});
+    // Create a write stream for the log file
+    var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
-app.use('/', indexRoutes);
-app.use('/api', apiRoutes);
+    // Create a middleware to save log data to the secondary MongoDB database
+
+    app.use((req, res, next) => {
+
+        const responseTime = res.get('response-time')
+        const responseTimeData = `${responseTime} ms`
+        const logData = {
+            remoteAddr: req.ip,
+            method: req.method,
+            url: req.originalUrl,
+            status: res.statusCode,
+            responseTime: responseTimeData,
+            userAgent: req.get('user-agent'),
+            timestamp: moment().tz(nepalTimeZone).format('YYYY-MM-DD HH:mm:ss'),
+        };
+
+        // Collection name accesslogs
+        const secondaryCollection = secondaryDB.collection('accessLogs');
+        secondaryCollection.insertOne(logData, (error, result) => {
+            if (error) {
+                console.error('Error saving log data to the secondary database:', error);
+            } else {
+                console.log('Log data saved to the secondary database:', result);
+            }
+        });
+
+        next();
+    });
+
+    app.use(morgan(customFormat, {
+        stream: accessLogStream
+    }));
+
+    console.log('Worker started with PID => ' + process.pid);
+
+    /* request rate limiter */
+    // const limiter = rateLimit({
+    //     windowMs: 15 * 60 * 1000,
+    //     max: 100,
+    // });
+
+    app.use('/', indexRoutes);
+    app.use('/api', apiRoutes);
+
+    // Start the Express app on a specific port
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app;
